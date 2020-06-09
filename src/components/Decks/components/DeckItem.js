9f8996d87afb60491525e50d5bd3d277eae31d82
Adilson Mandlate <adilsonmandlate@gmail.com>
2020-06-09 19:05:22 +0200

    feat: Add details to deck card

--- src/components/Decks/components/DeckItem.js
+++ src/components/Decks/components/DeckItem.js
@@ -1,19 +1,35 @@
 import React, { Fragment } from "react";
 import styled from "styled-components/native";
+import { Text } from "react-native";
 
 const Button = styled.TouchableHighlight`
   padding: 15px 20px 15px;
   border-bottom-width: 1px;
   border-color: #ecf0f1;
-`;
-
-const DeckName = styled.Text`
-  font-size: 18px;
-  font-weight: 500;
+  height: 200px;
+  border-radius: 5px;
+  border-width: 1px;
   margin-bottom: 5px;
 `;
 
-const DecksCards = styled.Text`
+const DeckMain = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const DeckName = styled.Text`
+  font-size: 17px;
+  font-weight: 700;
+  margin-bottom: 5px;
+`;
+
+const DeckDescription = styled.Text`
+  text-align: justify;
+  font-size: 15px;
+  flex: 1;
+`;
+
+const DeckDetails = styled.Text`
   color: #95a5a6;
 `;
 
@@ -21,10 +37,21 @@ const DeckItem = ({ deck, onPress }) => {
   return (
     <Button underlayColor={"#ecf0f1"} onPress={() => onPress(deck?.title)}>
       <Fragment>
-        <DeckName>{deck?.title}</DeckName>
-        <DecksCards>{`${deck?.questions?.length} ${
-          deck?.questions?.length > 1 ? "decks" : "deck"
-        }`}</DecksCards>
+        <DeckMain>
+          <DeckName>{deck?.title}</DeckName>
+          <Text style={{ color: "#bdc3c7" }}>30 mins ago</Text>
+        </DeckMain>
+
+        <DeckDescription>
+          A small description, lorem ipsum dolor sit amet, consectetur
+          adipiscing elit. Etiam malesuada justo lectus, nec pellentesque diam
+          tempor pharetra.
+        </DeckDescription>
+        <DeckDetails>{`${deck?.questions?.length} ${
+          deck?.questions?.length > 1 || deck?.questions?.length == 0
+            ? "cards"
+            : "card"
+        }`}</DeckDetails>
       </Fragment>
     </Button>
   );
